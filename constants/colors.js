efa342fc30b9f4fe69767e33475b36b005bf1edd
koleks92 <koleks92@gmail.com>
2023-10-08 20:59:42 +0200

    Colors temp

--- constants/colors.js
+++ constants/colors.js
@@ -1,1 +1,11 @@
 // CONSTANTS COLORS
+
+export const Colors = {
+    primary100: '#34A853', // Deep green (background)
+    primary200: '#21923f', // Darker green (navbar)
+    buttons: '#FF4136', // Tomato Red (accents, buttons)
+    text: '#FFFFFF', // White
+    icons: '#F5F5F5', // Light gray
+    success: '#4CAF50', // Green colors for succress messages
+    error: '#FF0000', // Red color for error messages
+}
