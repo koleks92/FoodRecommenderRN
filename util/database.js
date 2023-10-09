c5bd7bf1b4c77d3281a45811c62152dd4e7f9dc0
koleks92 <koleks92@gmail.com>
2023-10-09 22:36:27 +0200

    insertMeal function

--- util/database.js
+++ util/database.js
@@ -1,5 +1,6 @@
 // DATABASE FILE
 import * as SQlite from "expo-sqlite";
+import { Meal } from "../models/meal";
 
 const database = SQlite.openDatabase("meals.db");
 
@@ -20,14 +21,32 @@ export function init() {
             )`,
         [],
         () => {
-            resolve();
+          resolve();
         },
         (_, error) => {
-            reject(error);
+          reject(error);
         }
       );
     });
   });
 
   return promise;
+}
+
+export function insertMeal(meal) {
+  const promise = new Promise((resolve, reject) => {
+    database.transaction((tx) => {
+      tx.executeSql(
+        `INSERT INTO meals (title, type, imageUri, cusine, restaurant, price, description, recipe) VALUES (?, ?, ?, ?, ?, ?, ? ,?)`,
+        [meal.title, meal.type, meal.imageUri, meal.cuisne, meal.restaurant, meal.price, meal.description, meal.recipe ],
+        (_, result) => {
+          console.log(result);
+          resolve(result)
+        },
+        (_, error) => {
+          reject(error)
+        }
+      );
+    });
+  });
 }
