6b7024219c161b1821c794374e107f16949042fd
koleks92 <koleks92@gmail.com>
2023-10-09 11:07:32 +0200

    Tab Navigation no styling

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 
-export default function App() {
+import AddMeal from "./screens/AddMeal";
+import AllMeals from "./screens/AllMeals";
+import HomeMeals from "./screens/HomeMeals";
+import Recommender from "./screens/Recommender";
+import TakeawayMeals from "./screens/TakeawayMeals";
+import MealDetails from "./screens/MealDetails";
+
+const Stack = createNativeStackNavigator();
+const Tab = createMaterialBottomTabNavigator();
+
+function MealsOverview() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tab.Navigator>
+      <Tab.Screen name="AllMeals" component={AllMeals} />
+      <Tab.Screen name="HomeMeals" component={HomeMeals} />
+      <Tab.Screen name="TakeawayMeals" component={TakeawayMeals} />
+      <Tab.Screen name="Recommender" component={Recommender} />
+    </Tab.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <>
+      <StatusBar style="auto" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="MealsOverview" component={MealsOverview} />
+          <Stack.Screen name="AddMeal" component={AddMeal} />
+          <Stack.Screen name="MealDetails" component={MealDetails} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({});
