0b8d2af6c7c32916b8c1d21526d0f908ebc7f903
koleks92 <koleks92@gmail.com>
2023-10-09 22:28:40 +0200

    Meal class model

--- models/meal.js
+++ models/meal.js
@@ -1,1 +1,24 @@
 // MEAL CLASS MODEL
+export class Meal {
+  constructor(
+    id,
+    title,
+    type,
+    imageUri = false,
+    cusine = false,
+    restaurant = false,
+    price = false,
+    description = false,
+    recipe = false
+  ) {
+    this.id = id;
+    this.title = title;
+    this.type = type;
+    this.imageUri = imageUri;
+    this.cusine = cusine;
+    this.restaurant = restaurant;
+    this.price = price;
+    this.description = description;
+    this.recipe = recipe
+  }
+}
