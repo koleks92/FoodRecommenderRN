e5be1b6402c1b7a7648154375cc04fce1be15def
koleks92 <koleks92@gmail.com>
2023-10-09 11:06:33 +0200

    MealDetail template

---
+++ screens/MealDetails.js
@@ -0,0 +1,13 @@
+// Meal Details page
+
+import { View, Text } from "react-native";
+
+function MealDetails(id) {
+  return (
+    <View>
+      <Text>MealDetails {id}</Text>
+    </View>
+  );
+}
+
+export default MealDetails;
