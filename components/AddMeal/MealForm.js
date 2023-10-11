e3afa32b01a9a69c160c0e8bb35be8b6c5a2b8cf
koleks92 <koleks92@gmail.com>
2023-10-11 16:49:18 +0200

    Buttons handlers, missing styling

--- components/AddMeal/MealForm.js
+++ components/AddMeal/MealForm.js
@@ -1,25 +1,67 @@
-import { View, Text, StyleSheet} from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import { Colors } from "../../constants/colors";
+import Button from "../UI/Button";
+import { useEffect, useState } from "react";
 
 function MealForm() {
-    return (
-        <View>
-            <Text style={styles.titleText}> Meal Form</Text>
-        </View>
-    )
-};
+  const [choice, setChoice] = useState("");
+
+  function choiceTakeawayHandler() {
+    setChoice("t");
+  }
+
+  function choiceHomeHandler() {
+    setChoice("h");
+  }
+
+  useEffect(() => {
+    console.log(choice);
+  }, [choice]);
+
+  return (
+    <View style={styles.root}>
+      <View style={styles.buttonContainer}>
+        <Button
+          style={styles.button}
+          textStyle={styles.buttonText}
+          onPress={choiceTakeawayHandler}
+        >
+          Takeaway
+        </Button>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button
+          style={styles.button}
+          textStyle={styles.buttonText}
+          onPress={choiceHomeHandler}
+        >
+          Make at home
+        </Button>
+      </View>
+    </View>
+  );
+}
 
 export default MealForm;
 
 const styles = StyleSheet.create({
-    root: {
-      flex: 1,
-      backgroundColor: Colors.background,
-      opacity: 0.99,
-      padding: 24
-    },
-    titleText: {
-        color: Colors.text,
-        fontSize: 16
-    }
-  })
+  root: {
+    flex: 1,
+    justifyContent: 'space-evenly',
+  },
+  buttonContainer: {
+    width: "100%",
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: "2%"
+  },
+  button: {
+    width: "100%",
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    fontSize: 28,
+  },
+});
