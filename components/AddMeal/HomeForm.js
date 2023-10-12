413154b9c34a65d8f551fbdff64bdf64369c2d9f
koleks92 <koleks92@gmail.com>
2023-10-12 11:20:22 +0200

    Initail form

---
+++ components/AddMeal/HomeForm.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+function HomeForm() {
+    return(
+        <View>
+            <Text> HomeForm </Text>
+        </View>
+    )
+};
+
+export default HomeForm;
---
+++ components/AddMeal/TakeawayForm.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+function TakeawayForm() {
+    return(
+        <View>
+            <Text> Takeaway </Text>
+        </View>
+    )
+};
+
+export default TakeawayForm;
