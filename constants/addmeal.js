8efc7e04f133417007acac5623969f90669d0619
koleks92 <koleks92@gmail.com>
2023-10-12 11:54:09 +0200

    price constant

---
+++ constants/addmeal.js
@@ -0,0 +1,42 @@
+export const cusine = [
+  "Italian",
+  "Greek",
+  "Spanish",
+  "Japanese",
+  "Indian",
+  "Mexican",
+  "Turkish",
+  "American",
+  "French",
+  "Peruvian",
+  "Chinese",
+  "Brazilian",
+  "Portuguese",
+  "Polish",
+  "German",
+  "Indonesian",
+  "Coratian",
+  "Argentinian",
+  "Korean",
+  "Vietnamese",
+  "Hungarian",
+  "Romanian",
+  "Filipino",
+  "Iranian",
+  "Serbian",
+  "Georgian",
+  'Czech',
+  'Bulgarian',
+  'English',
+  'Thai'
+];
+
+export const price = [
+  'Budget-Friendly',
+  'Affordable',
+  'Mid-Range',
+  'Premium',
+  'Luxury',
+  'Exclusive'
+
+]
