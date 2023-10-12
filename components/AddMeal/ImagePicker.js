ffcc0bd72ee825978f83cd5cae1801da86e63a22
koleks92 <koleks92@gmail.com>
2023-10-12 19:49:31 +0200

    ImagePicker function

---
+++ components/AddMeal/ImagePicker.js
@@ -0,0 +1,98 @@
+import { View, Alert, Image, Text, StyleSheet } from "react-native";
+import {
+  launchCameraAsync,
+  useCameraPermissions,
+  PermissionStatus,
+} from "expo-image-picker";
+import { useState } from "react";
+import { Colors } from "../../constants/colors";
+import Button from "../UI/Button";
+
+function ImagePicker({ onImageTaken }) {
+  const [cameraPermissionInformation, requestPermission] =
+    useCameraPermissions();
+  const [cameraPhoto, setCameraPhoto] = useState();
+
+  async function verifyPermissions() {
+    if (cameraPermissionInformation.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission();
+
+      return permissionResponse.granted;
+    }
+
+    if (cameraPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        "Insufficient Permissions!",
+        "You need to grant camera permissions to use this app."
+      );
+      return false;
+    }
+
+    return true;
+  }
+
+  async function takeImageHandler() {
+    const hasPermission = await verifyPermissions();
+
+    if (!hasPermission) {
+      return;
+    }
+
+    const image = await launchCameraAsync({
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 0.5,
+    });
+    setCameraPhoto(image.assets[0]);
+    onImageTaken(image.assets[0]);
+  }
+
+  let imagePreview = <Text>No image taken yet.</Text>;
+
+  if (cameraPhoto) {
+    imagePreview = (
+      <Image source={{ uri: cameraPhoto.uri }} style={styles.image} />
+    );
+  }
+
+  return (
+    <View>
+      <View style={styles.imagePreview}>{imagePreview}</View>
+      <Button
+        buttonStyle={styles.button}
+        textStyle={styles.buttonText}
+        onPress={takeImageHandler}
+      >
+        Take Image
+      </Button>
+    </View>
+  );
+}
+
+export default ImagePicker;
+
+const styles = StyleSheet.create({
+  imagePreview: {
+    height: 200,
+    width: "100%",
+    marginBottom: 8,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.primary200,
+    borderRadius: 4,
+    elevation: 4
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+  button: {
+    marginVertical: 8,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonText: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});
