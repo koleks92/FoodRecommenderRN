01adc823b8ec075e5498c6666c30260cd3a1503c
koleks92 <koleks92@gmail.com>
2023-10-12 11:21:10 +0200

    ScreenView handler

--- screens/AddMeal.js
+++ screens/AddMeal.js
@@ -3,12 +3,35 @@
 import { StyleSheet, View } from "react-native";
 import ChoiceForm from "../components/AddMeal/ChoiceForm";
 import Background from "../components/UI/Background";
+import { useState, useEffect } from "react";
+import HomeForm from "../components/AddMeal/HomeForm";
+import TakeawayForm from "../components/AddMeal/TakeawayForm";
 
 function AddMeals() {
+  const [choice, setChoice] = useState('');
+
+  function handleChoiceData(data) {
+    setChoice(data);
+  }
+
+  useEffect(() => {
+    console.log(choice);
+  }, [choice]);
+
+  let ScreenView;
+  
+  if (choice === '') {
+    ScreenView = <ChoiceForm handleChoiceData={handleChoiceData}/>
+  } else if (choice === 'h') {
+    ScreenView = <HomeForm />
+  } else if (choice === 't') {
+    ScreenView = <TakeawayForm />
+  }
+
   return (
     <Background>
       <View style={styles.container}>
-        <ChoiceForm />
+        {ScreenView}
       </View>
     </Background>
   );
