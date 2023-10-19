6c9a74373602f9465abb33162bbc6dd5d37a3152
koleks92 <koleks92@gmail.com>
2023-10-19 19:32:55 +0200

    Extra styling option

--- components/UI/IconButton.js
+++ components/UI/IconButton.js
@@ -1,13 +1,13 @@
 import { Pressable, View, StyleSheet } from "react-native";
 import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
 
-function IconButton({ icon, size, color, onPress}) {
+function IconButton({ icon, size, color, onPress, style}) {
     return (
       <Pressable
         onPress={onPress}
         style={({ pressed }) => (pressed ? styles.pressed : null)}
       >
-        <View style={styles.buttonContainer}>
+        <View style={[styles.buttonContainer, style ? style : null]}>
           <Ionicons name={icon} size={size} color={color} />
         </View>
       </Pressable>
@@ -19,9 +19,8 @@ export default IconButton;
 const styles = StyleSheet.create({
   buttonContainer: {
     borderRadius: 24,
-    padding: 6,
-    marginHorizontal: 8,
-    marginVertical: 2,
+    marginHorizontal: 12,
+    marginVertical: 6,
   },
   pressed: {
     opacity: 0.75,
