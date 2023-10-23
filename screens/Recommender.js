fbf80c5c33bd1c2baa63fcafadebaa42d6b0ee0d
koleks92 <koleks92@gmail.com>
2023-10-23 20:52:35 +0200

    ScreenView logic

--- screens/Recommender.js
+++ screens/Recommender.js
@@ -3,15 +3,26 @@
 import { View, Text, StyleSheet } from "react-native";
 import Background from "../components/UI/Background";
 import RecommenderForm from "../components/Recommender/RecommenderForm";
+import { useState } from "react";
 
 function Recommender() {
-  function getOptionsHandler(options) {
-    console.log(options);
+  const [options, setOptions] = useState("");
+
+  function getOptionsHandler(searchOptions) {
+    setOptions(searchOptions);
+  }
+
+  let ScreenView;
+
+  if (options === "") {
+    ScreenView = <RecommenderForm setSearchOptions={getOptionsHandler} />;
+  } else {
+    ScreenView = <Text>Result</Text>
   }
 
   return (
     <Background>
-      <RecommenderForm setSearchOptions={getOptionsHandler}/>
+      {ScreenView}
     </Background>
   );
 }
