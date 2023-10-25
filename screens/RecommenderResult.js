9e2e0dea4a1404b88d12f8ce34f3cce788701457
koleks92 <koleks92@gmail.com>
2023-10-25 11:39:48 +0200

    Screen draft

---
+++ screens/RecommenderResult.js
@@ -0,0 +1,15 @@
+import { View, Text, StyleSheet } from "react-native";
+
+function RecommenderResult({ meal }) {
+    return (
+        <View>
+            <Text> Result</Text>
+        </View>
+    )
+}
+
+export default RecommenderResult;
+
+const styles = StyleSheet.create({
+
+})
