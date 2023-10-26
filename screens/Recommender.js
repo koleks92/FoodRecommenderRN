6524c8cd4d7ab91eb3b4ee334a367a823813a20a
koleks92 <koleks92@gmail.com>
2023-10-26 11:05:05 +0200

    Add isFocused to reload allMeals

--- screens/Recommender.js
+++ screens/Recommender.js
@@ -4,23 +4,24 @@ import Background from "../components/UI/Background";
 import RecommenderForm from "../components/Recommender/RecommenderForm";
 import { useEffect, useLayoutEffect, useState } from "react";
 import { fetchAllMeals } from "../util/database";
-import {
-  getRecommendation,
-  mealRecommender,
-  oneMealRandom,
-} from "../util/recommenderFunctions";
+import { getRecommendation } from "../util/recommenderFunctions";
+import { useIsFocused } from "@react-navigation/native";
 
 function Recommender({ navigation }) {
-  const [allMeals, SetAllMeals] = useState();
+  const [allMeals, setAllMeals] = useState();
+  const isFocused = useIsFocused();
 
   // Load meals using async fetchMeals
-  useLayoutEffect(() => {
-    async function loadMeals() {
+  useEffect(() => {
+    async function loadAllMeals() {
       const meals = await fetchAllMeals();
-      SetAllMeals(meals);
+      setAllMeals(meals);
     }
-    loadMeals();
-  }, []);
+
+    if (isFocused) {
+      loadAllMeals();
+    }
+  }, [isFocused]);
 
   function getRecommendationHandler(searchOptions) {
     const meal = getRecommendation(allMeals, searchOptions);
@@ -29,7 +30,7 @@ function Recommender({ navigation }) {
     navigation.navigate("RecommenderResult", {
       meal: meal,
       searchOptions: searchOptions,
-      allMeals: allMeals
+      allMeals: allMeals,
     });
   }
 
